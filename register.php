7447af2a0bf4e8f40c7789ffc1bd3dbbdd9773b0
Afonso Fernandes <up202207987@up.pt>
2024-04-17 10:29:59 +0100

    logi and registor and css implemented

--- register.php
+++ register.php
@@ -1,36 +1,30 @@
 <?php
-      require_once('database/connection.php');
-      require_once('database/categories.php');
-      require_once('templates/common.php');
-      require_once('templates/display_categories.php');
-  
-      $db = getDatabaseConnection();
-      $categories = getAllCategories($db);
-      output_header();
-      display_categories($categories);
+    require_once('database/connection.php');
+    require_once('database/categories.php');
+    require_once('templates/common.php');
+    require_once('templates/display_categories.php');
+
+    $db = getDatabaseConnection();
+    $categories = getAllCategories($db);
+    output_header();
+    display_categories($categories);
 ?>
 
-
-<section>
-    <h2>Register</h2>
-    <form action="action_register.php" method="post">
-        <label for="username">Username:</label><br>
-        <input type="text" id="username" name="username" required><br>
-        <label for="name">Name:</label><br>
-        <input type="text" id="name" name="name" required><br>
-        <label for="email">Email:</label><br>
-        <input type="email" id="email" name="email" required><br>
-        <label>Phone number: <input type="text" name="phonenumber" required></label><br>
-        <label for="password">Password:</label><br>
-        <input type="password" id="password" name="password" required><br><br>
-        <label>Confirm Password: <input type="password" name="cpassword" required></label><br>
-        <button type="submit">Register</button>
-
-    
-        
-      
-    </form>
+<section id="login_register" >
+    <article id="register_txt">
+        <h1>Welcome!</h1>
+        <p>Please fill in this form to create an account.</p>
+    </article>
+    <article id="form-container register">
+        <form action="action_register.php" method="post">
+            <input type="text" id="username" name="username" placeholder="Username" required><br>
+            <input type="text" id="name" name="name" placeholder="Name" required><br>
+            <input type="email" id="email" name="email" placeholder="Email" required><br>
+            <input type="password" id="password" name="password" placeholder="Password" required><br>
+            <input type="password" id="cpassword" name="cpassword" placeholder="Confirm Password" required><br><br>
+            <button type="submit">Register</button>
+        </form>
+    </article>
 </section>
-
 
 <?php output_footer();?>
