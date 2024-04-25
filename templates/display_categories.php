e5c4941e6e7b4014cadbd6815719a4cb97ba8ff4
Afonso Fernandes <up202207987@up.pt>
2024-04-25 13:35:26 +0100

    categorys buttoon implemented and working. searchbar reponsive is a problem

--- templates/display_categories.php
+++ templates/display_categories.php
@@ -2,6 +2,9 @@
 function display_categories($categories){
 ?>
 
+<input type="checkbox" id="hamburger"> 
+<label class="hamburger" for="hamburger"></label>
+
 <ul id="Category_filters">
     <?php foreach($categories as $categorie){?>
             <li><a href = ""><?= $categorie['category_name']?></a></li>
